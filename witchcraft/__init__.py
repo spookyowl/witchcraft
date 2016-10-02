052305190259fb996f4b6ae537fdf44953362d5b
Peter Facka <pfacka@hexcells.com>
2016-10-02 12:07:26 +0200

    bugfix: inverted condition

--- witchcraft/__init__.py
+++ witchcraft/__init__.py
@@ -1,9 +1,10 @@
 from witchcraft.upsert import prepare_table, upsert_data
+from witchcraft.combinators import query, template
 
 
 def upsert(connection, schema_name, table_name, data_points, primary_keys):
 
-    if len(data_points):
+    if len(data_points) == 0:
         return
 
     prepare_table(connection, schema_name, table_name, data_points, primary_keys)
