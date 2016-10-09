c3e4ff8119d81e2feee52018443a19ba14a6d24c
pfacka <peter.facka@amico.sk>
2016-10-09 12:49:36 +0200

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="witchcraft",
-    version="0.2.1",
+    version="0.2.2",
     description='',
     author='Peter Facka',
     author_email='pfacka@spookyowl.com',
@@ -13,12 +13,12 @@ setup(
     ],
     zip_safe=False,	
     install_requires=[
-	'psycopg2>=2.6.2',
+	'psycopg2>=2.6.1',
 	'SQLAlchemy>=1.0.6',
         'hy>=0.11.1',
         'pyparsing>=2.1.1'
     ],
-    provides=['witchcraft (0.2.1)'],
+    provides=['witchcraft (0.2.2)'],
     include_package_data=True,
     classifiers=[
       'Development Status :: 3 - Alpha',
