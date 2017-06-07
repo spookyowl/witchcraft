8d24f2eb0ae2aad80de5c4826310f8d12b889e04
pfacka <peter.facka@amico.sk>
2017-06-07 12:19:38 +0200

    data

---
+++ witchcraft/queries/psql_upsert_update.sql
@@ -0,0 +1,15 @@
+UPDATE :schema_name.:table_name
+  SET 
+    :(map
+   (fn [column] 
+      (+ "\"" column "\"" "= data_update.\"" column "\""))
+   column_names)
+
+FROM data_update     
+WHERE
+1 = 1
+:(reduce
+   (fn [memo pkey]
+     (+ memo " AND " schema-name "." table-name "." pkey "= data_update." pkey))
+   primary_keys "")
+;
