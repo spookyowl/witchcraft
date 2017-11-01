e12c75d4bd051d4ae2c5417c532e3f55a08840b0
peter.facka <peter.facka@friday.de>
2017-11-01 11:57:07 +0100

    release 0.2.15

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="witchcraft",
-    version="0.2.14",
+    version="0.2.15",
     description='',
     author='Peter Facka',
     author_email='pfacka@spookyowl.com',
