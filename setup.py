46780f2f88c4e2f5415c0b8aed2b8d25ec2c21ac
peter.facka <peter.facka@friday.de>
2018-09-14 16:27:10 +0200

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="witchcraft",
-    version="0.2.26",
+    version="0.2.27",
     description='',
     author='Peter Facka',
     author_email='pfacka@spookyowl.com',
