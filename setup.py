994d8c2a87eaa1008a4e8c59115aa38971854145
peter.facka <peter.facka@friday.de>
2020-12-14 10:54:40 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="witchcraft",
-    version="0.2.41",
+    version="0.2.43",
     description='',
     author='Peter Facka',
     author_email='pfacka@spookyowl.com',
